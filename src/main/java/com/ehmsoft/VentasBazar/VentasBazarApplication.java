f21c26db698d34f94fd87fbdb658fc8918be0a8f
EmilioM1983 <emiliomayergit@gmail.com>
2025-02-24 06:44:59 -0300

    Proyecto back de sistema de registro de ventas de bazar

---
+++ src/main/java/com/ehmsoft/VentasBazar/VentasBazarApplication.java
@@ -0,0 +1,13 @@
+package com.ehmsoft.VentasBazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasBazarApplication.class, args);
+	}
+
+}
